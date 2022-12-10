ad02b95196ac3299379b089a2043b2f095dad966
Masterjx9 <82799287+Masterjx9@users.noreply.github.com>
2022-12-10 14:01:55 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup, find_packages
 import warnings
 
 setup(
-    name='aws-tagger-uiuc',
-    version='0.6.4',
+    name='aws-tagger',
+    version='0.6.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'boto3>=1.4.4',
-        'botocore>=1.5.7',
+        'boto3',
+        'botocore',
         'click>=6.6',
         'docutils>=0.13.1',
-        'futures>=3.0.5',
         'jmespath>=0.9.1',
         'retrying>=1.3.3',
         's3transfer>=0.1.10',
@@ -23,10 +22,10 @@ setup(
             "aws-tagger=tagger.cli:cli",
         ]
     },
-    author="Eric L. Epps, modified from v0.6.3 from Patrick Cullen and the WaPo platform tools team",
-    author_email="eepps2@illinois.edu",
-    url="https://github.com/ericepps-uiuc/aws-tagger",
-    download_url = "https://github.com/ericepps-uiuc/aws-tagger/tarball/v0.6.4",
+    author="Jay Justice with Datakind along with, Eric L. Epps, Patrick Cullen and the WaPo platform tools team",
+    author_email="it@datakind.org",
+    url="https://github.com/datakind/aws-tagger", 
+    download_url = "https://github.com/datakind/aws-tagger.git",
     keywords = ['tag', 'tagger', 'tagging', 'aws'],
     classifiers = []
 )
