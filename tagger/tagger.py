8a80cddabe103e99a1730e95f4ba21fbffb5c222
Masterjx9 <82799287+Masterjx9@users.noreply.github.com>
2022-12-10 21:12:00 -0800

    Route Table, Security Group, Subnet, & VPC Tagging
    Route Table, Security Group, Subnet, & VPC Tagging functionality has been added

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -435,19 +435,57 @@ class RouteTableTagger(object):
         self.verbose = verbose
         self.rtb = _client('ec2', role=role, region=region)
 
+    def tag(self, instance_id, tags):
+        aws_tags = _dict_to_aws_tags(tags)
+        print(aws_tags)
+        resource_ids = [instance_id]
+        if self.verbose:
+            print("tagging %s with %s" % (", ".join(resource_ids), _format_dict(tags)))
+        if not self.dryrun:
+            try:
+                self._RouteTable_create_tags(Resources=resource_ids, Tags=aws_tags)
+            except botocore.exceptions.ClientError as exception:
+                if exception.response["Error"]["Code"] in ['InvalidSnapshot.NotFound', 'InvalidVolume.NotFound', 'InvalidInstanceID.NotFound']:
+                    print("EC2 Route Table Resource not found: %s" % instance_id)
+                else:
+                    raise exception
+
     @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
     def _describe_RouteTable(self, **kwargs):
         return self.rtb.describe_route_tables(**kwargs)
+
+    @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
+    def _RouteTable_create_tags(self, **kwargs):
+        return self.rtb.create_tags(**kwargs)
 
 class SecurityGroupTagger(object):
     def __init__(self, dryrun, verbose, role=None, region=None):
         self.dryrun = dryrun
         self.verbose = verbose
         self.sg = _client('ec2', role=role, region=region)
+    
+    def tag(self, instance_id, tags):
+        aws_tags = _dict_to_aws_tags(tags)
+        print(aws_tags)
+        resource_ids = [instance_id]
+        if self.verbose:
+            print("tagging %s with %s" % (", ".join(resource_ids), _format_dict(tags)))
+        if not self.dryrun:
+            try:
+                self._SecurityGroup_create_tags(Resources=resource_ids, Tags=aws_tags)
+            except botocore.exceptions.ClientError as exception:
+                if exception.response["Error"]["Code"] in ['InvalidSnapshot.NotFound', 'InvalidVolume.NotFound', 'InvalidInstanceID.NotFound']:
+                    print("EC2 Security Group Resource not found: %s" % instance_id)
+                else:
+                    raise exception
 
     @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
     def _describe_SecurityGroup(self, **kwargs):
         return self.sg.describe_security_groups(**kwargs)
+
+    @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
+    def _SecurityGroup_create_tags(self, **kwargs):
+        return self.sg.create_tags(**kwargs)
 
 class SubnetTagger(object):
     def __init__(self, dryrun, verbose, role=None, region=None):
@@ -455,9 +493,28 @@ class SubnetTagger(object):
         self.verbose = verbose
         self.subnet = _client('ec2', role=role, region=region)
 
+    def tag(self, instance_id, tags):
+        aws_tags = _dict_to_aws_tags(tags)
+        print(aws_tags)
+        resource_ids = [instance_id]
+        if self.verbose:
+            print("tagging %s with %s" % (", ".join(resource_ids), _format_dict(tags)))
+        if not self.dryrun:
+            try:
+                self._Subnet_create_tags(Resources=resource_ids, Tags=aws_tags)
+            except botocore.exceptions.ClientError as exception:
+                if exception.response["Error"]["Code"] in ['InvalidSnapshot.NotFound', 'InvalidVolume.NotFound', 'InvalidInstanceID.NotFound']:
+                    print("EC2 Subnet Resource not found: %s" % instance_id)
+                else:
+                    raise exception
+
     @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
     def _describe_Subnet(self, **kwargs):
         return self.subnet.describe_subnets(**kwargs)
+
+    @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
+    def _Subnet_create_tags(self, **kwargs):
+        return self.subnet.create_tags(**kwargs)
 
 class VPCTagger(object):
     def __init__(self, dryrun, verbose, role=None, region=None):
@@ -465,9 +522,28 @@ class VPCTagger(object):
         self.verbose = verbose
         self.vpc = _client('ec2', role=role, region=region)
 
+    def tag(self, instance_id, tags):
+        aws_tags = _dict_to_aws_tags(tags)
+        print(aws_tags)
+        resource_ids = [instance_id]
+        if self.verbose:
+            print("tagging %s with %s" % (", ".join(resource_ids), _format_dict(tags)))
+        if not self.dryrun:
+            try:
+                self._VPC_create_tags(Resources=resource_ids, Tags=aws_tags)
+            except botocore.exceptions.ClientError as exception:
+                if exception.response["Error"]["Code"] in ['InvalidSnapshot.NotFound', 'InvalidVolume.NotFound', 'InvalidInstanceID.NotFound']:
+                    print("EC2 VPC Resource not found: %s" % instance_id)
+                else:
+                    raise exception
+
     @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
     def _describe_VPC(self, **kwargs):
         return self.vpc.describe_vpcs(**kwargs)
+
+    @retry(retry_on_exception=_is_retryable_exception, stop_max_delay=30000, wait_exponential_multiplier=1000)
+    def _VPC_create_tags(self, **kwargs):
+        return self.vpc.create_tags(**kwargs)
 
 class EFSTagger(object):
     def __init__(self, dryrun, verbose, role=None, region=None):
