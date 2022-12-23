1c14ae7f58013e5acb4add03368f362b6b24e826
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2022-12-23 15:06:22 -0800

    Sketched out implementation of kafka cluster. Should be very similar to awsLambda implementation

--- tagger/services/kafka/service.py
+++ tagger/services/kafka/service.py
@@ -1,17 +1,28 @@
-from tagger.sconfig import _client, _dict_to_aws_tags, _format_dict, _is_retryable_exception
+from tagger.sconfig import _client, _dict_to_aws_tags, _format_dict, _is_retryable_exception, _name_to_arn
 import botocore
 from retrying import retry
+
+# NOTE: Kafka implementation is not finished
 
 class kafkaTagger(object):
     def __init__(self, dryrun, verbose, role=None, region=None):
         self.dryrun = dryrun
         self.verbose = verbose
-        self.ami = _client('ec2', role=role, region=region)
+        self.role = role
+        self.region = region
+        self.kafka = _client('kafka', role=role, region=region)
 
     def tag(self, instance_id, tags):
         aws_tags = _dict_to_aws_tags(tags)
         print(aws_tags)
         resource_ids = [instance_id]
+        sts = _client('sts', role=self.role, region=self.region)
+        account_id = self.sts.get_caller_identity()["Account"]
+        service = 'kafka'
+        # Note: need to specify between kafka clusters and connectors
+        resource_arn = f'cluster:{resource_ids[0]}'
+        file_system_id = _name_to_arn(resource_ids[0])
+
         if self.verbose:
             print("tagging %s with %s" % (", ".join(resource_ids), _format_dict(tags)))
         if not self.dryrun:
